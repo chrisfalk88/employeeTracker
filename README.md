1d32460bb145c09303af63d1090967cf4d90dcb1
Chris Falk <christophersfalk@gmail.com>
2020-10-29 10:14:47 -0700

    added add department functionality

--- README.md
+++ README.md
@@ -4,13 +4,19 @@
 
 ## Psudeo Code 
     /////////////////// TO DO \\\\\\\\\\\\\\\\\
+    * add some default employyes
 
-    * add branches for first inquirer prompt "view, add, update and exit
+    * write code add departments, role, emps
+
+    *write code to update existing emp roles
+    
    
    *write functions to update Employee, etc 
 
 
     /////////////////// DONE \\\\\\\\\\\\\\\\\
+    *write connection querries to display dep role and  emps
+       * add branches for first inquirer prompt "view, add, update and exit
      * create welcome print with inqurier prompt to display first list actions 
     * add boiler plate mysql connections 
     * NPM init and install mysql and inquirer 
--- tracker.js
+++ tracker.js
@@ -1,6 +1,7 @@
 //Dependancies 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { SSL_OP_EPHEMERAL_RSA } = require("constants");
 
 // My SQL Connection
 var connection = mysql.createConnection({
@@ -22,24 +23,26 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
   });
 
-function main() {
-    //Prints out ASCII Welcome Page
-    console.log(
+  //Prints out ASCII Welcome Page
+  console.log(
         
 
-  " _____                 _                         __  __ \n" +                                  
-  "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___  |  \\/  | __ _ _ __   __ _  __ _  ___ _ __ \n" +
-  "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \  | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__| \n" +
-  "| |___| | | | | | |_) | | (_) | |_| |  __/  __/ | |  | | (_| | | | | (_| | (_| |  __/ | \n" +
-  "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_| \n" + 
-  "                |_|            |___/                                      |___/  \n"        
+    " _____                 _                         __  __ \n" +                                  
+    "| ____|_ __ ___  _ __ | | ___  _   _  ___  ___  |  \\/  | __ _ _ __   __ _  __ _  ___ _ __ \n" +
+    "|  _| | '_ ` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \  | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '__| \n" +
+    "| |___| | | | | | |_) | | (_) | |_| |  __/  __/ | |  | | (_| | | | | (_| | (_| |  __/ | \n" +
+    "|_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___| |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_| \n" + 
+    "                |_|            |___/                                      |___/  \n"        
+  
+      );
+function main() {
+    
 
-    );
 
 
   inquirer.prompt([
       {
-          input: "list",
+          type: "list",
           message: "What would you like to do?",
           choices: ["View","Add","Update","Exit"],
           name: "list"
@@ -51,8 +54,74 @@ function main() {
         connection.end();
     } else if (answers.list === "View") {
         //ask what would like to be viewed, departments, roles emps
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "What would you like to view?",
+                choices: ["Departments", "Roles", "Employees"],
+                name: "view"
+            }
+        ]).then(function(answers){
+            if (answers.view === "Departments") {
+                //write sql to display all departments 
+                connection.query("SELECT * FROM departments", function(err, res){
+                    if (err) throw err; 
+                    console.table(res);
+                    main()
+                })
+            } else if (answers.view === "Roles") {
+                //write sql to display all Roles
+                connection.query("SELECT * FROM role", function(err, res){
+                    if (err) throw err; 
+                    console.table(res);
+                    main()
+                })
+            } else if (answers.view === "Employees") {
+                //write sql to display all employees
+                connection.query("SELECT * FROM employee", function(err, res){
+                    if (err) throw err; 
+                    console.table(res);
+                    printTable("employee");
+                    main()
+                });
+            }
+        }) //end of VIEW 
     } else if (answers.list === "Add") {
         //ask what would like to be added, departments, roles emps
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "What would you like to add?",
+                choices: ["Departments", "Roles", "Employee"],
+                name: "add"
+
+            }
+        ]).then(function(answers){
+            if (answers.add === "Departments") {
+                //sql to add into departments 
+
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "Please enter in new Department",
+                        name : "depart"
+                    }
+                ]).then(function(answers){
+                    connection.query("INSERT INTO departments SET ?",
+                    {
+                        name: answers.depart
+                    }, function(err) {
+                        if (err) throw err;
+                        console.log(`${answers.depart} was successfully entered into department table`)
+                    }
+                    )
+                })
+            } else if (answers.add === "Roles") {
+                //sql to add into roles 
+            } else if (answers.add === "Employee") {
+                //sql to add employee here 
+            }
+        })
     } else if (answers.list === "Update") {
         //ask what would like to be updated, departments, roles emps
     }
@@ -60,5 +129,16 @@ function main() {
   }) 
 
 } //end of main function
+
+
+//ask hannah about making this funciton work 
+function printTable(data){
+    connection.query("SELECT * FROM " + (data), function(err, res){
+        if (err) throw err; 
+        console.table(res);
+        main()
+    });
+}
+
 
 main();
